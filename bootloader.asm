f7784eb5a5a1e6c9b3344f2ae59181ef09ca6c00
Yamen1294 <black.cat221bck@gmail.com>
2026-06-29 05:25:56 +0300

    Add files via upload

---
+++ bootloader.asm
@@ -0,0 +1,20 @@
+bit 16
+org 0x7c00 ;start at this memo address
+
+mov si,0
+
+print:
+    mov ah, 0x0e
+    mov al, [hello + si]
+    int 0x10 ;video servecies
+    add si,1
+    cmp byte [hello + si],0
+    jne print
+
+jmp $
+
+hello:
+    db "Hello there welcome to pc",0
+
+times 510 - ($ - $$) db 0
+dw 0xAA55 ; finish boot sector
